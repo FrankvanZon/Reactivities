c44e8c174b6fe0b0ab60bd09a8876ef8eff05fa5
Frank van Zon <f.vanzon@outlook.comm>
2025-01-24 20:33:11 +0100

    End of stage 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,24 +1,14 @@
+
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); // Connection to the database
-});
-
-builder.Services.AddCors(opt =>{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddApplicationService(builder.Configuration);
 
 var app = builder.Build();
 
